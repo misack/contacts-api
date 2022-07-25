853de49da0a55712e6e0dfcabdd167e2f724aec3
Macheyeki Isack <misack@users.noreply.github.com>
2022-07-25 18:59:41 +0300

    requirements freeze and settings  cors

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    "corsheaders",
      
     'authentication',
     'contacts',
@@ -69,12 +70,21 @@ SWAGGER_SETTINGS = {
 }
 
 
+# cors white list ip
+# previously CORS_ORIGIN_WHITELIST
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+]
+
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
